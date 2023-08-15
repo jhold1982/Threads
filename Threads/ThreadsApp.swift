0ccb1c2ad4098ed2324975c67c8edc8d1c395dfd
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-08-15 08:39:21 -0500

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Justin Hold on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
